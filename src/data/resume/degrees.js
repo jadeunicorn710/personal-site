732f34bfb6e260bb444594b2896bbad90d1c2ced
yqi82 <yqi82@gatech.edu>
2022-04-08 23:21:16 -0400

    updated education

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -1,19 +1,19 @@
 const degrees = [
   {
     school: 'Georgia Institute of Technology',
-    degree: 'M.S. Computer Science',
+    degree: 'M.S. Computer Science, top-10 program in US, 3.90/4.0 GPA',
     link: 'https://www.gatech.edu/',
     year: 2021,
   },
   {
     school: 'University of Illinois Urbana-Champaign',
-    degree: 'M.S. Civil Engineering',
+    degree: 'M.S. Civil Engineering, top-2 program in US',
     link: 'https://illinois.edu/',
     year: 2015,
   },
   {
     school: 'Xi\'an University of Architecture & Technology',
-    degree: 'B.S. Civil Engineering',
+    degree: 'B.S. Civil Engineering, graduated with honor',
     link: 'https://www.xauat.edu.cn/en/',
     year: 2013,
   },
