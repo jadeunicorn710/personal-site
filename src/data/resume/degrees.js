e6af8ce43d5b07bb25b029aece0d56b854670853
qiyunlin <qiyunlin710@qiyunlins-MacBook-Pro.local>
2026-04-14 20:21:35 -0700

    updates

--- src/data/resume/degrees.js
+++ src/data/resume/degrees.js
@@ -3,19 +3,6 @@ const degrees = [
     school: 'Georgia Institute of Technology',
     degree: 'M.S. Computer Science, top10 program in US, 3.90/4.0 GPA',
     link: 'https://www.gatech.edu/',
-    year: 2021,
-  },
-  {
-    school: 'University of Illinois Urbana-Champaign',
-    degree: 'M.S. Civil Engineering, top2 program in US',
-    link: 'https://illinois.edu/',
-    year: 2015,
-  },
-  {
-    school: 'Xi\'an University of Architecture & Technology',
-    degree: 'B.S. Civil Engineering, graduated with honor',
-    link: 'https://www.xauat.edu.cn/en/',
-    year: 2013,
   },
 ];
 
